ba44783de97ba9dc7923d985bec053f5c6bce9e3
Patrick Cerny <patrick.cerny04@gmail.com>
2021-08-08 18:03:59 +0200

    added ui lib

--- apps/alpsbte-api/src/main.ts
+++ apps/alpsbte-api/src/main.ts
@@ -1,18 +1,11 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
 import * as express from 'express';
 
 const app = express();
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to alpsbte-api!' });
-});
-
 const port = process.env.port || 3333;
+
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
+
 server.on('error', console.error);
