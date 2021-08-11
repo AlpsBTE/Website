1be97dd1717882a7d7f93e544548ab32c2985036
Patrick Cerny <patrick.cerny04@gmail.com>
2021-08-12 00:46:12 +0200

    fixed structure and added Home-Lib

--- libs/home/src/lib/home.spec.tsx
+++ libs/home/src/lib/home.spec.tsx
@@ -2,11 +2,9 @@ import { render } from '@testing-library/react';
 
 import Home from './home';
 
-describe('Loading', () => {
+describe('Home', () => {
   it('should render successfully', () => {
     const { baseElement } = render(<Home />);
-
     expect(baseElement).toBeTruthy();
   });
-
 });
