ef484eacf6350e41459500ebe91d72a10be8c4e1
Georg-code <jornig@outlook.com>
2021-08-11 20:35:57 +0200

    not finished! (+Routing +Lazy Loading )

---
+++ apps/alpsbte-web/src/routes/home/home.tsx
@@ -0,0 +1,10 @@
+
+export function Home() {
+  return (
+    <div className="Home">
+          <p>thä hom paisch ist not rädi ät thä moumend</p>
+    </div>
+  );
+}
+
+export default Home;
---
+++ apps/alpsbte-web/src/routes/loading/loading.tsx
@@ -0,0 +1,12 @@
+// TODO Add clean looking spinner? -> Check with design departement
+export function Loading() {
+  return (
+    <div className="Loading">
+            <p>loading..</p>
+
+       </div>
+       
+  );
+}
+
+export default Loading;
