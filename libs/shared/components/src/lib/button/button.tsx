d724cc71ab6c870beb0e0698a9648b20c501eb95
Georg <jornig@outlook.com>
2021-08-15 18:03:56 +0200

    just button styling

--- libs/shared/components/src/lib/button/button.tsx
+++ libs/shared/components/src/lib/button/button.tsx
@@ -6,16 +6,15 @@ export interface ButtonProps {
   onClick: () => void;
   disabled?: boolean;
   size: 'lg' | 'md' | 'sm';
-  color: string;
 }
 
 export function Button(props: ButtonProps) {
+
   return (
     <button
       disabled={props.disabled}
       className={'button button__' + props.size}
       onClick={props.onClick}
-      style={{ backgroundColor: props.color }}
     >
       {props.label}
 
