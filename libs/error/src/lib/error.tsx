925e17cdb9403a43103dafc88d9cb7ed7abef024
Patrick Cerny <patrick.cerny04@gmail.com>
2021-08-20 16:04:47 +0200

    added error page

---
+++ libs/error/src/lib/error.tsx
@@ -0,0 +1,14 @@
+import './error.module.scss';
+
+/* eslint-disable-next-line */
+export interface ErrorProps {}
+
+export function Error(props: ErrorProps) {
+  return (
+    <div>
+      <h1>Welcome to Error!</h1>
+    </div>
+  );
+}
+
+export default Error;
