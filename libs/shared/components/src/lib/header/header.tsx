aa1297149e3fc608bd3c60fb49678752f365b5b5
Patrick Cerny <patrick.cerny04@gmail.com>
2021-08-20 16:42:24 +0200

    added header

--- libs/shared/components/src/lib/header/header.tsx
+++ libs/shared/components/src/lib/header/header.tsx
@@ -1,13 +1,37 @@
+import { Link } from 'react-router-dom';
 import './header.module.scss';
 
 /* eslint-disable-next-line */
-export interface HeaderProps {}
+
+interface navItems {
+  text: string;
+  to: string;
+}
+export interface HeaderProps {
+  headerText: string;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  headerIcon: any;
+  navItems: navItems[];
+}
 
 export function Header(props: HeaderProps) {
   return (
-    <div>
-      <h1>Welcome to Header!</h1>
-    </div>
+    <nav className="header__">
+      <div className="header__image-container">
+        <img src={props.headerIcon} alt={props.headerText} />
+      </div>
+      <ul className="header__navitem-container">
+        {props.navItems.map((_, key) => {
+          return (
+            <Link to={props.navItems[key].to}>
+              <li className="header__navitem_container__item">
+                {props.navItems[key].to}
+              </li>
+            </Link>
+          );
+        })}
+      </ul>
+    </nav>
   );
 }
 
