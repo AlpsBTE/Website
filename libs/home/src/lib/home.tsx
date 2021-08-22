5fdec4d2c887d78c407e786f1bd21af1671ce834
Patrick Cerny <patrick.cerny04@gmail.com>
2021-08-23 00:05:07 +0200

    moved header to app.tsx

--- libs/home/src/lib/home.tsx
+++ libs/home/src/lib/home.tsx
@@ -5,24 +5,9 @@ import HomeContentSection from '../components/home-content-section/home-content-
 /* eslint-disable-next-line */
 export interface HomeProps {}
 
-const headerProps: HeaderProps = {
-  headerIcon:
-    'https://yt3.ggpht.com/ytc/AKedOLQ5MRqFQDZEnX4QQZZbtYh_dqp680D-an9ZIfTM=s900-c-k-c0x00ffffff-no-rj',
-  forceColor: false,
-  headerText: 'Alps BTE',
-  navItems: [
-    { text: 'About Us', to: '/aboutUs' },
-    { text: 'Gallery', to: '/gallery' },
-    { text: 'Downloads', to: '/downloads' },
-    { text: 'FAQ', to: '/faq' },
-    { text: 'Application', to: '/application' },
-    { text: 'Contact', to: '/contact' },
-  ],
-};
 export function Home(props: HomeProps) {
   return (
     <>
-      <Header {...headerProps}></Header>
       <div className="heading">
         <div className="block">
           <div className="head_line_box">
