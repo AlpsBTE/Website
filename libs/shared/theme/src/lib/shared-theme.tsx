8aa5984d405c0adb711bfb94bd4c57bcc2d08a2b
Behemoth <simonostini@gmail.com>
2021-08-25 15:22:28 +0200

    first tries :(

---
+++ libs/shared/theme/src/lib/shared-theme.tsx
@@ -0,0 +1,14 @@
+import './shared-theme.module.scss';
+
+/* eslint-disable*/
+export interface SharedThemeProps {}
+
+export function SharedTheme(props: SharedThemeProps) {
+  return (
+    <div>
+      <h1>Welcome to SharedTheme!</h1>
+    </div>
+  );
+}
+
+export default SharedTheme;
