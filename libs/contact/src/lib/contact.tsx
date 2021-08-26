f26d2f813d5bf333cf03f64d1b94ec19fd065391
Georg <jornig@outlook.com>
2021-08-26 20:33:23 +0200

    added section

--- libs/contact/src/lib/contact.tsx
+++ libs/contact/src/lib/contact.tsx
@@ -1,36 +1,69 @@
 /* eslint-disable*/
 import './contact.scss';
 import { PageTitle } from '@alpsbte/shared/components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faInstagram, faYoutube, faTwitter, faTiktok, faReddit } from '@fortawesome/free-brands-svg-icons'
+
 
 export interface ContactProps {}
 
 export function Contact(props: ContactProps) {
-  return (
-    <div className="contact">
-      <PageTitle title="Contact" subtitle="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren," />
+return (
+<div className="contact">
+  <PageTitle title="Contact"
+    subtitle="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren," />
 
-      <div className="contact__container">
+  <div className="contact__container">
 
-        <div className="contact__social-media">
-          <h2>Social Media</h2>
-          <div className="contact__social-icons">
-            <div className="contact__icon"></div>
-            <div className="contact__icon"></div>
-            <div className="contact__icon"></div>
-            <div className="contact__icon"></div>
-            <div className="contact__icon"></div>
-            <div className="contact__icon"></div>
-    
-          </div>
+    <div className="contact__social-media">
+      <h2>Social Media</h2>
+      <div className="contact__social-icons">
+        <div className="contact__social-media__icon">
+          <FontAwesomeIcon icon={faInstagram} size='lg' />
         </div>
-
-        <div className="contact__outreach">
-          Mensch
+        <div className="contact__social-media__icon">
+          <FontAwesomeIcon icon={faYoutube} size='lg' />
+        </div>
+        <div className="contact__social-media__icon">
+          <FontAwesomeIcon icon={faReddit} size='lg' />
+        </div>
+        <div className="contact__social-media__icon">
+          <FontAwesomeIcon icon={faTwitter} size='lg' />
+        </div>
+        <div className="contact__social-media__icon">
+          <FontAwesomeIcon icon={faTiktok} size='lg' />
         </div>
 
       </div>
     </div>
-  );
+
+    <div className="contact__people">
+      <div>
+        <div className="contact__people__container">
+          <h2>Outreach</h2>
+          <p>Gartensocke</p>
+            <p>Email <a href="mailto:gartensocke@alps-bte.com">gartensocke@alps-bte.com</a></p>
+            <p>Discord <a href="https://discord.com/invite/vgkspay">Gartensocke#1525</a></p>
+          </div>
+          <div className="contact__people__container">
+          <h2>Outreach</h2>
+          <p>Gartensocke</p>
+            <p>Email <a href="mailto:gartensocke@alps-bte.com">gartensocke@alps-bte.com</a></p>
+            <p>Discord <a href="https://discord.com/invite/vgkspay">Gartensocke#1525</a></p>
+          </div>
+          <div className="contact__people__container">
+          <h2>Outreach</h2>
+          <p>Gartensocke</p>
+            <p>Email <a href="mailto:gartensocke@alps-bte.com">gartensocke@alps-bte.com</a></p>
+            <p>Discord <a href="https://discord.com/invite/vgkspay">Gartensocke#1525</a></p>
+        </div>
+        
+      </div>
+    </div>
+
+  </div>
+</div>
+);
 }
 
 export default Contact;
