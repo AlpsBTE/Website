d4d12177512624a4f2cb0fd407103d57a95b7ec1
Georg <jornig@outlook.com>
2021-08-26 13:27:46 +0200

    social media tab

--- libs/contact/src/lib/contact.tsx
+++ libs/contact/src/lib/contact.tsx
@@ -1,13 +1,34 @@
 /* eslint-disable*/
-import './contact.module.scss';
+import './contact.scss';
 import { PageTitle } from '@alpsbte/shared/components';
 
 export interface ContactProps {}
 
 export function Contact(props: ContactProps) {
   return (
-    <div>
-       <PageTitle title="Contact" subtitle="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,"/>
+    <div className="contact">
+      <PageTitle title="Contact" subtitle="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren," />
+
+      <div className="contact__container">
+
+        <div className="contact__social-media">
+          <h2>Social Media</h2>
+          <div className="contact__social-icons">
+            <div className="contact__icon"></div>
+            <div className="contact__icon"></div>
+            <div className="contact__icon"></div>
+            <div className="contact__icon"></div>
+            <div className="contact__icon"></div>
+            <div className="contact__icon"></div>
+    
+          </div>
+        </div>
+
+        <div className="contact__outreach">
+          Mensch
+        </div>
+
+      </div>
     </div>
   );
 }
