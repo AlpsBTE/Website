9f8eafe8bdc9671ab52cb1bdb82a74535650afc2
Georg <jornig@outlook.com>
2021-08-28 16:36:10 +0200

    added functionality

--- libs/faq/src/components/accordeon/accordeon.tsx
+++ libs/faq/src/components/accordeon/accordeon.tsx
@@ -1,14 +1,42 @@
+import { useState } from 'react';
 import './accordeon.scss';
 
 /* eslint-disable-next-line */
-export interface AccordeonProps {}
+export interface AccordeonProps {
+title: string,
+content: string,
+open?: boolean,
+}
 
 export function Accordeon(props: AccordeonProps) {
-  return (
-    <div>
-      <h1>Welcome to Accordeon!</h1>
+const [open, setOpen] = useState(props.open ? true : false);
+
+function accordeon__handleClick(e : any) {
+e.preventDefault();
+  
+  if (open) {
+    setOpen(false)
+  } else {
+    setOpen(true)
+  }
+  console.log(open);
+}
+  
+return (
+    <div className='accordeon'>
+    <button onClick={accordeon__handleClick} className="accordeon__container">
+    <div className="accordeon__container">
+      <p>{props.title}</p>
     </div>
-  );
+    </button>
+      <div className="accordeon__content" style={{display: open ? 'inline' : 'none'}}>
+        <p>{props.content}</p>
+    </div>
+      
+   
+    </div>
+      
+);
 }
 
 export default Accordeon;
--- libs/faq/src/lib/faq.tsx
+++ libs/faq/src/lib/faq.tsx
@@ -1,13 +1,16 @@
 /* eslint-disable */
 import './faq.scss';
 import { PageTitle } from '@alpsbte/shared/components';
+import Accordeon from '../components/accordeon/accordeon';
 
 export interface FaqProps {}
 
 export function Faq(props: FaqProps) {
   return (
     <div>
-     <PageTitle title="FAQ" subtitle="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,"/>
+      <PageTitle title="FAQ" subtitle="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren," />
+      
+      <Accordeon title="Warum ist AlpsBTE das beste Bauteam" content="Alles spricht dafür"></Accordeon>
     </div>
   );
 }
