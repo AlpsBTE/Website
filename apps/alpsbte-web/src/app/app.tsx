e682b4c2f9f0d1df1d812a56949aef25705d0f8a
Behemoth <simonostini@gmail.com>
2021-08-31 11:49:23 +0200

    corrections and store architecture setup; added react strict mode

--- apps/alpsbte-web/src/app/app.tsx
+++ apps/alpsbte-web/src/app/app.tsx
@@ -1,7 +1,22 @@
+import React from 'react';
 import * as fromRouter from '@alpsbte/router';
+import { Provider as StoreProvider } from 'mobx-react';
+
+// stores
+import { languageStore } from '@alpsbte/shared/stores';
+
+const stores = {
+  languageStore,
+};
 
 export const App = () => {
-  return <fromRouter.Router />;
+  return (
+    <React.StrictMode>
+      <StoreProvider {...stores}>
+        <fromRouter.Router />
+      </StoreProvider>
+    </React.StrictMode>
+  );
 };
 
 export default App;
