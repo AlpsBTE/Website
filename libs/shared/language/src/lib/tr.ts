a28d62555f5a3349ae9dc23096f440f8989c69cf
Behemoth <simonostini@gmail.com>
2021-09-01 18:51:09 +0200

    improved translation types

--- libs/shared/language/src/lib/tr.ts
+++ libs/shared/language/src/lib/tr.ts
@@ -3,26 +3,31 @@ import { languageStore } from '@alpsbte/shared/stores';
 import { deepFind } from '@alpsbte/shared/util';
 import { ISet } from './interfaces/set';
 
+export type Divider = '.';
+export type Empty = '';
+
 // Limiter is needed because type computations could be very complex or infinite
 type Limiter = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ...0[]];
 
-type Join<K, P> = K extends string | number
+type TranslationKeyJoin<K, P> = K extends string | number
   ? P extends string | number
-    ? `${K}${'' extends P ? '' : '.'}${P}`
+    ? `${K}${Empty extends P ? Empty : Divider}${P}`
     : never
   : never;
 
-type Paths<T, D extends number = 10> = [D] extends [never]
+type TranslationPathFactory<T, D extends number = 10> = [D] extends [never]
   ? never
   : T extends object
   ? {
       [K in keyof T]-?: K extends string | number
-        ? `${K}` | Join<K, Paths<T[K], Limiter[D]>>
+        ?
+            | `${K}`
+            | TranslationKeyJoin<K, TranslationPathFactory<T[K], Limiter[D]>>
         : never;
     }[keyof T]
-  : '';
+  : Empty;
 
-export type TranslationPath = Paths<typeof languageStore.set>;
+export type TranslationPath = TranslationPathFactory<typeof languageStore.set>;
 
 export function tr(key: TranslationPath): string {
   const target = deepFind<ISet>(languageStore.set, key);
