0dfc2a676558e32e68723043fa0a60fa06ba5e22
Behemoth <simonostini@gmail.com>
2021-09-01 13:24:05 +0200

    improved return types

--- libs/shared/language/src/lib/testdata/testdata.ts
+++ libs/shared/language/src/lib/testdata/testdata.ts
@@ -1,7 +1,8 @@
+import { ISet } from '../interfaces/set';
 import { Language } from '../types/language';
 
-export const fetchTestdata = async (language: Language): Promise<any> => {
-  return (await import(`../testdata/${language}.json`)).default;
+export const fetchTestdata = async (language: Language): Promise<ISet> => {
+  return <ISet>(await import(`../testdata/${language}.json`)).default;
 };
 
 export default fetchTestdata;
