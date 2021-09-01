1e88fa7c58a740a375608c4a78c6b5bcdd06c060
Behemoth <simonostini@gmail.com>
2021-09-01 19:16:18 +0200

    fixed custom error test

--- libs/shared/util/src/lib/error/error.spec.ts
+++ libs/shared/util/src/lib/error/error.spec.ts
@@ -1,7 +1,7 @@
 import { throwError } from './error';
 
 describe('throwError', () => {
-  it('should shrow a custom error message', () => {
-    expect(throwError('this is a test error')).toThrowError();
+  it('should throw a custom error message', () => {
+    expect(throwError).toThrow(Error);
   });
 });
