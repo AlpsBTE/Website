a1d0e092769ac3d3ccab83b82cd0a0cbe89e824b
Behemoth <simonostini@gmail.com>
2021-09-02 22:40:15 +0200

    added dev security error

--- libs/shared/util/src/lib/error/error.ts
+++ libs/shared/util/src/lib/error/error.ts
@@ -1,3 +1,8 @@
+/* eslint-disable */
+export enum InternalError {
+  missingTranslation = 'Missing Translation',
+}
+
 /**
  * @param message The error message that will display on throw
  * @returns never
