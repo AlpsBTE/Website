4424f1abc1ec9f5f3912d6f77a85d3b2700e4854
Georg <jornig@outlook.com>
2021-09-02 09:12:41 +0200

    fixed rotating

--- libs/faq/src/components/accordeon/accordeon.tsx
+++ libs/faq/src/components/accordeon/accordeon.tsx
@@ -1,41 +1,47 @@
 import { CSSProperties, useState } from 'react';
 import './accordeon.scss';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronUp } from '@fortawesome/free-solid-svg-icons'
+import { faChevronUp } from '@fortawesome/free-solid-svg-icons';
 
 /* eslint-disable-next-line */
 export interface AccordeonProps {
-title: string,
-content: string,
-open?: boolean,
-style?: CSSProperties,
+  title: string;
+  content: string;
+  open?: boolean;
+  style?: CSSProperties;
 }
 
 export function Accordeon(props: AccordeonProps) {
-const [open, setOpen] = useState(props.open ? true : false);
+  const [open, setOpen] = useState(props.open ? true : false);
 
-function accordeon__toggle(e : any) {
-e.preventDefault();
+  function accordeon__toggle(e: any) {
+    e.preventDefault();
 
-setOpen(open?false:true)
-}
+    setOpen(open ? false : true);
+  }
 
-return (
-<div className="accordion">
-  <div className="accordion__title" onClick={accordeon__toggle}>
-    <span className="accordeon__title">{props.title}</span>
-    <span className="accordion__icon" style={{...props.style, rotate: open?'360deg':'180deg'}}>
-      <FontAwesomeIcon icon={faChevronUp} />
-    </span>
+  //  + open ? '360deg' : '180deg' +
 
-    <div className="accordion__content" aria-expanded={!open}>
-      <p className="accordion__content__text">{props.content}</p>
+  return (
+    <div className="accordion">
+      <div className="accordion__title" onClick={accordeon__toggle}>
+        <span className="accordeon__title">{props.title}</span>
+        <span
+          className="accordion__icon"
+          style={{
+            ...props.style,
+            transform: open ? 'rotate(360deg)' : 'rotate(180deg)',
+          }}
+        >
+          <FontAwesomeIcon icon={faChevronUp} />
+        </span>
 
+        <div className="accordion__content" aria-expanded={!open}>
+          <p className="accordion__content__text">{props.content}</p>
+        </div>
+      </div>
     </div>
-  </div>
-
-</div>
-);
+  );
 }
 
 export default Accordeon;
