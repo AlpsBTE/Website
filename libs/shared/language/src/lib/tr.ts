d088637bb8570ab3659b8c40a4876ea903007099
Behemoth <simonostini@gmail.com>
2021-09-02 16:45:01 +0200

    modified translationexports

--- libs/shared/language/src/lib/tr.ts
+++ libs/shared/language/src/lib/tr.ts
@@ -3,20 +3,22 @@ import { languageStore } from '@alpsbte/shared/stores';
 import { deepFind, throwError } from '@alpsbte/shared/util';
 import { ISet } from './interfaces/set';
 
-export type Divider = '.';
-export type ___ = '';
-export type Limit = 10;
+type Divider = '.';
+type ___ = '';
+type Limit = 10;
 
 // Limiter is needed because type computations could be very complex or infinite
 type Limiter = [never, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, ...0[]];
 
-type TranslationKeyJoin<K, P> = K extends string | number
+export type TranslationKeyJoin<K, P> = K extends string | number
   ? P extends string | number
     ? `${K}${___ extends P ? ___ : Divider}${P}`
     : never
   : never;
 
-type TranslationPathFactory<T, D extends number = Limit> = [D] extends [never]
+export type TranslationPathFactory<T, D extends number = Limit> = [D] extends [
+  never
+]
   ? never
   : T extends { [Z in string | number]: any }
   ? {
