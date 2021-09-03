11d28369fcc9e06eff2363ddbea3196daf8515b0
Georg <jornig@outlook.com>
2021-09-03 20:13:02 +0200

    sturcture

--- libs/about-us/src/lib/about-us.tsx
+++ libs/about-us/src/lib/about-us.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable*/
-import { Route, Link } from 'react-router-dom';
+import { Button } from '@alpsbte/shared/components';
 import { PageTitle } from '@alpsbte/shared/components';
 
 import './about-us.scss';
@@ -13,6 +13,26 @@ export function AboutUs(props: AboutUsProps) {
         title="Press"
         subtitle="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,"
       />
+
+      <p className="aboutus__text">
+        Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
+        eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+        voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet
+        clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit
+        amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+        nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat,
+        sed diam voluptua. At vero eos et accusam et justo duo dolores et ea
+        rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+        ipsum dolor sit amet.
+      </p>
+
+      <Button label="Contact us" />
+
+      <div className="aboutus_image-container">
+        <div className="aboutus_image-container">
+          <img className="aboutus_image" />
+        </div>
+      </div>
     </div>
   );
 }
