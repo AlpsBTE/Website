0837cdb0e45ddc7a399a8e4888acd26488b7a122
Georg <jornig@outlook.com>
2021-09-03 14:46:30 +0200

    init about us page

--- libs/about-us/src/lib/about-us.tsx
+++ libs/about-us/src/lib/about-us.tsx
@@ -2,15 +2,17 @@
 import { Route, Link } from 'react-router-dom';
 import { PageTitle } from '@alpsbte/shared/components';
 
-import './about-us.module.scss';
+import './about-us.scss';
 
 export interface AboutUsProps {}
 
 export function AboutUs(props: AboutUsProps) {
   return (
     <div>
-      <PageTitle title="Press" subtitle="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,"/>
-
+      <PageTitle
+        title="Press"
+        subtitle="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,"
+      />
     </div>
   );
 }
