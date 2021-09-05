eef19b8123191e8e511ec8c37f905be543c7a2c2
Georg <jornig@outlook.com>
2021-09-05 17:06:18 +0200

    chnaged page title props

--- libs/about-us/src/lib/about-us.tsx
+++ libs/about-us/src/lib/about-us.tsx
@@ -9,10 +9,7 @@ export interface AboutUsProps {}
 export function AboutUs(props: AboutUsProps) {
   return (
     <div>
-      <PageTitle
-        title="Press"
-        subtitle="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,"
-      />
+      <PageTitle title="About us" />
 
       <p className="aboutus__text">
         Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy
