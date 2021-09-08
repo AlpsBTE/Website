b0cb72a4ae02e783e6fac739e34c1ad2048d7531
Georg-code <jornig@outlook.com>
2021-09-08 21:05:09 +0200

    init press page

---
+++ libs/press/src/lib/press.tsx
@@ -0,0 +1,14 @@
+import './press.scss';
+
+/* eslint-disable-next-line */
+export interface PressProps {}
+
+export function Press(props: PressProps) {
+  return (
+    <div>
+      <h1>Welcome to Press!</h1>
+    </div>
+  );
+}
+
+export default Press;
