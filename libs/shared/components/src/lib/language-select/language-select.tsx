28d36135f24a781a30cdec3a3339b43b0e76ecbb
Behemoth <simonostini@gmail.com>
2021-09-10 23:04:39 +0200

    added better styles

--- libs/shared/components/src/lib/language-select/language-select.tsx
+++ libs/shared/components/src/lib/language-select/language-select.tsx
@@ -5,6 +5,8 @@ import type { Language } from '@alpsbte/shared/language';
 import { inject, observer } from 'mobx-react';
 import { languageStore } from '@alpsbte/shared/stores';
 import { ChangeEvent } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGlobe } from '@fortawesome/free-solid-svg-icons';
 
 export interface LanguageSelectProps {}
 
@@ -13,20 +15,27 @@ export const LanguageSelect: React.FC<LanguageSelectProps> = inject(
 )(
   observer(({}: LanguageSelectProps) => {
     return (
-      <select
-        className="language-select"
-        onChange={(e: ChangeEvent<HTMLSelectElement>) =>
-          languageStore.setLanguage(e.target.value as Language)
-        }
-      >
-        {languages.map((o: string, i: number) => {
-          return (
-            <option key={i} value={o}>
-              {o}
-            </option>
-          );
-        })}
-      </select>
+      <div className="language-select">
+        <FontAwesomeIcon
+          icon={faGlobe}
+          size="lg"
+          className="language-select__icon"
+        />
+        <select
+          className="language-select__select"
+          onChange={(e: ChangeEvent<HTMLSelectElement>) =>
+            languageStore.setLanguage(e.target.value as Language)
+          }
+        >
+          {languages.map((o: string, i: number) => {
+            return (
+              <option key={i} value={o}>
+                {o}
+              </option>
+            );
+          })}
+        </select>
+      </div>
     );
   })
 );
