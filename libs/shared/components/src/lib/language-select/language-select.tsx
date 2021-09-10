bd2f42b40f9e674f6b3221d51a3c599ff420e190
Behemoth <simonostini@gmail.com>
2021-09-10 22:58:56 +0200

    update

--- libs/shared/components/src/lib/language-select/language-select.tsx
+++ libs/shared/components/src/lib/language-select/language-select.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable */
 import './language-select.scss';
-import { LanguageEnum, languages } from '@alpsbte/shared/language';
+import { languages } from '@alpsbte/shared/language';
 import type { Language } from '@alpsbte/shared/language';
 import { inject, observer } from 'mobx-react';
 import { languageStore } from '@alpsbte/shared/stores';
@@ -13,54 +13,20 @@ export const LanguageSelect: React.FC<LanguageSelectProps> = inject(
 )(
   observer(({}: LanguageSelectProps) => {
     return (
-      <div
-        className={`${`language-select`} ${
-          languageStore.isSelectOpened ? 'language-select-opened' : ''
-        }`}
+      <select
+        className="language-select"
+        onChange={(e: ChangeEvent<HTMLSelectElement>) =>
+          languageStore.setLanguage(e.target.value as Language)
+        }
       >
-        {/* <ul className="language-select__ul">
-          <li
-            className="language-select__ul__active"
-            onClick={() =>
-              languageStore.setIsSelectedOpened(
-                languageStore.isSelectOpened ? false : true
-              )
-            }
-          >
-            {languageStore.language}
-          </li>
-          {languageStore.isSelectOpened &&
-            languages
-              .filter((l) => l !== languageStore.language)
-              .map((o) => {
-                return (
-                  <li
-                    className="language-select__ul__inactive"
-                    onClick={() => {
-                      languageStore.setIsSelectedOpened(false);
-                      languageStore.setLanguage(o as Language);
-                    }}
-                  >
-                    {o}
-                  </li>
-                );
-              })}
-        </ul> */}
-        <select
-          className="language-select"
-          onChange={(e: ChangeEvent<HTMLSelectElement>) =>
-            languageStore.setLanguage(e.target.value as Language)
-          }
-        >
-          {languages.map((o: string, i: number) => {
-            return (
-              <option key={i} value={o}>
-                {o}
-              </option>
-            );
-          })}
-        </select>
-      </div>
+        {languages.map((o: string, i: number) => {
+          return (
+            <option key={i} value={o}>
+              {o}
+            </option>
+          );
+        })}
+      </select>
     );
   })
 );
