1cd027e7b97e83fcc7fa16c23b04557c00c4e9c2
Behemoth <simonostini@gmail.com>
2021-09-11 17:55:30 +0200

    updated styles

--- libs/loader/src/lib/loader.tsx
+++ libs/loader/src/lib/loader.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable */
-import React, { useEffect, useState } from 'react';
-import { useDelayUnmount } from '@alpsbte/shared/util';
+import React from 'react';
 import './loader.scss';
 
 const possibilities = [
@@ -39,21 +38,10 @@ const animation =
   possibilities[Math.floor(Math.random() * possibilities.length)];
 
 export const Loader: React.FC = () => {
-  const [dots, setDots] = useState<number>(0);
-  useEffect(() => {
-    setTimeout(() => setDots(dots === 3 ? 0 : dots + 1), 300);
-  }, [dots]);
-
-  const test = new Array(dots).fill('.');
-  console.log(test);
-
   return (
     <div className="loader">
       <div className="loader__content">
-        <h3>
-          Loading
-          {new Array(dots).fill('.').map((e) => e)}
-        </h3>
+        <h2>Loading</h2>
         <img src={require(`../assets/${animation}.gif`)} alt={animation} />
       </div>
     </div>
