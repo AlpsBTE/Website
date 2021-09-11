a81ebd6aac1b1bfa59a453f00935e16d06642756
Behemoth <simonostini@gmail.com>
2021-09-11 16:15:00 +0200

    implemented randomness

--- libs/loader/src/lib/loader.tsx
+++ libs/loader/src/lib/loader.tsx
@@ -1,24 +1,46 @@
 import React from 'react';
 import './loader.scss';
 
+const possibilities = [
+  'bat',
+  'bee',
+  'bells',
+  'blaze',
+  'chicken',
+  'cod',
+  'dolphin',
+  'end_crystal',
+  'fire',
+  'ghast',
+  'jeb',
+  'lava',
+  'magma_cube',
+  'panda',
+  'parrot',
+  'phantom',
+  'piston',
+  'polar_bear',
+  'rabbit',
+  'salmon',
+  'seagrass',
+  'shulkerbox',
+  'silverfish',
+  'squid',
+  'sticky_piston',
+  'torch',
+  'villager',
+  'wheat',
+  'wolf',
+].sort((a: string, b: string) => a.localeCompare(b));
+
 export const Loader: React.FC = () => {
+  const animation =
+    possibilities[Math.floor(Math.random() * possibilities.length)];
+  console.log(animation);
   return (
-    <div>
-      <svg
-        aria-hidden="true"
-        focusable="false"
-        data-prefix="fals"
-        data-icon="circle-notch"
-        className="loader"
-        role="img"
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 512 512"
-      >
-        <path
-          fill="currentColor"
-          d="M288 39.056v16.659c0 10.804 7.281 20.159 17.686 23.066C383.204 100.434 440 171.518 440 256c0 101.689-82.295 184-184 184-101.689 0-184-82.295-184-184 0-84.47 56.786-155.564 134.312-177.219C216.719 75.874 224 66.517 224 55.712V39.064c0-15.709-14.834-27.153-30.046-23.234C86.603 43.482 7.394 141.206 8.003 257.332c.72 137.052 111.477 246.956 248.531 246.667C393.255 503.711 504 392.788 504 256c0-115.633-79.14-212.779-186.211-240.236C302.678 11.889 288 23.456 288 39.056z"
-        ></path>
-      </svg>
+    <div className="loader">
+      {/* eslint-disable */}
+      <img src={require(`../assets/${animation}.gif`)} alt={'test'} />
     </div>
   );
 };
