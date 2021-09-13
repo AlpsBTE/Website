7c5bca4295061b12e14075312e0934fc1fb17d1d
Behemoth <simonostini@gmail.com>
2021-09-13 17:36:13 +0200

    init footer

---
+++ libs/shared/components/src/lib/footer/footer.tsx
@@ -0,0 +1,14 @@
+/* eslint-disable*/
+import './footer.module.scss';
+
+export interface FooterProps {}
+
+export const Footer = ({}: FooterProps) => {
+  return (
+    <footer className="footer">
+      <h1>Welcome to Footer!</h1>
+    </footer>
+  );
+};
+
+export default Footer;
