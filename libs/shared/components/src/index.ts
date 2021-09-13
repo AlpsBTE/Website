92883ab3468480925fba1eb3e17aac4bcdbf82a9
Behemoth <simonostini@gmail.com>
2021-09-13 17:56:55 +0200

    fixed imports

--- libs/shared/components/src/index.ts
+++ libs/shared/components/src/index.ts
@@ -3,3 +3,4 @@ export * from './lib/button/button';
 export * from './lib/page-title/page-title';
 export * from './lib/scroll-indicator/scroll-indicator';
 export * from './lib/footer/footer';
+export * from './lib/logo/logo';
