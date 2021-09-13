974864b2822bccf9b441c698e4dd156aac7ece1f
Georg-code <jornig@outlook.com>
2021-09-13 22:10:12 +0200

    gallery page init

--- libs/gallery/src/lib/gallery.tsx
+++ libs/gallery/src/lib/gallery.tsx
@@ -1,6 +1,5 @@
-
 /* eslint-disable*/
-import './gallery.module.scss';
+import './gallery.scss';
 import { PageTitle } from '@alpsbte/shared/components';
 
 export interface GalleryProps {}
@@ -8,7 +7,10 @@ export interface GalleryProps {}
 export function Gallery(props: GalleryProps) {
   return (
     <div>
-      <PageTitle title="Gallery" subtitle="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,"/>
+      <PageTitle
+        title="Gallery"
+        subtitle="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,"
+      />
     </div>
   );
 }
