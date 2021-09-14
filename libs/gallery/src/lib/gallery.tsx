14b6c98b56d429032acf0f9c24bc2664c3bb07dd
Georg-code <jornig@outlook.com>
2021-09-14 16:47:33 +0200

    added images

--- libs/gallery/src/lib/gallery.tsx
+++ libs/gallery/src/lib/gallery.tsx
@@ -11,6 +11,46 @@ export function Gallery(props: GalleryProps) {
         title="Gallery"
         subtitle="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,"
       />
+      <div className="gallery__container">
+        <div className="gallery__image-container">
+          <img
+            src="https://via.placeholder.com/300x200/"
+            className="gallery__image"
+          ></img>
+          <img
+            src="https://via.placeholder.com/300x200/"
+            className="gallery__image"
+          ></img>
+          <img
+            src="https://via.placeholder.com/300x200/"
+            className="gallery__image"
+          ></img>
+          <img
+            src="https://via.placeholder.com/300x200/"
+            className="gallery__image"
+          ></img>
+          <img
+            src="https://via.placeholder.com/300x200/"
+            className="gallery__image"
+          ></img>
+          <img
+            src="https://via.placeholder.com/300x200/"
+            className="gallery__image"
+          ></img>
+          <img
+            src="https://via.placeholder.com/300x200/"
+            className="gallery__image"
+          ></img>
+          <img
+            src="https://via.placeholder.com/300x200/"
+            className="gallery__image"
+          ></img>
+          <img
+            src="https://via.placeholder.com/300x200/"
+            className="gallery__image"
+          ></img>
+        </div>
+      </div>
     </div>
   );
 }
