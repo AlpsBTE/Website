a3d0aec0c8716a5865b770ba7b7d43ec26e00ba1
Behemoth <simonostini@gmail.com>
2021-09-17 19:08:10 +0200

    added contacts WIP

--- libs/shared/config/src/lib/contact.ts
+++ libs/shared/config/src/lib/contact.ts
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export enum StaffRole {
   Manager,
   Helper,
@@ -8,6 +9,52 @@ export enum StaffRole {
 export interface IStaff {
   discordUsername: string;
   role: StaffRole;
+  displayName: string;
+  email?: string;
+  /**
+   * Defines whether a staff member will be displayed on the website contact section
+   */
+  contactable?: boolean;
 }
 
-export const contact = <const>{};
+export const contact: { [name: string]: IStaff } = <const>{
+  cinnazeyy: {
+    discordUsername: 'Cinnazeyy#2440',
+    role: StaffRole.Manager,
+    displayName: 'Cinnazeyy',
+    email: 'cinnazeyy@alps-bte.com',
+    contactable: true,
+  },
+  r3tuxn: {
+    discordUsername: 'R3tuxn#7169',
+    role: StaffRole.Manager,
+    displayName: 'R3tuxn',
+    email: 'r3tuxn@alps-bte.com',
+    contactable: false,
+  },
+  kami: {
+    discordUsername: 'Kami#3480',
+    role: StaffRole.Manager,
+    displayName: 'Kami',
+    email: 'kami@alps-bte.com',
+    contactable: false,
+  },
+  behemoth: {
+    discordUsername: 'Behemoth#4026',
+    role: StaffRole.Developer,
+    displayName: 'Behemoth',
+    contactable: true,
+  },
+  hohi: {
+    discordUsername: 'Hohi#0675',
+    role: StaffRole.Developer,
+    displayName: 'Hohi',
+    contactable: true,
+  },
+  jokil: {
+    discordUsername: 'jo_kil#1977',
+    role: StaffRole.Developer,
+    displayName: 'jo_kil',
+    contactable: false,
+  },
+};
