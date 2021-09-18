ac41ddf21e2aaf968bb670ac2608a2054d414234
Behemoth <simonostini@gmail.com>
2021-09-18 19:45:16 +0200

    added content

--- libs/application/src/components/application-step/application-step.tsx
+++ libs/application/src/components/application-step/application-step.tsx
@@ -1,4 +1,6 @@
 /* eslint-disable */
+import { tr } from '@alpsbte/shared/language';
+import { CSSProperties } from 'react';
 import './application-step.scss';
 
 export interface ApplicationStepProps {
@@ -6,6 +8,7 @@ export interface ApplicationStepProps {
   title: string;
   description: string;
   img: string;
+  style?: CSSProperties;
 }
 
 export const ApplicationStep = ({
@@ -13,11 +16,18 @@ export const ApplicationStep = ({
   title,
   description,
   img,
+  style = {},
 }: ApplicationStepProps) => {
   return (
-    <div className="application-step">
-      <div className="application-stepp__img">
-        <img src="" alt="Application Step Discord" />
+    <div className="application-step" style={style}>
+      <div className="application-step__img">
+        <img src={img} alt="Application Step Discord" />
+      </div>
+      <div className="application-step__content">
+        <h2>
+          {stepNumber}. {title}
+        </h2>
+        <p>{description}</p>
       </div>
     </div>
   );
--- libs/application/src/lib/application.tsx
+++ libs/application/src/lib/application.tsx
@@ -3,6 +3,8 @@ import './application.scss';
 import { PageTitle } from '@alpsbte/shared/components';
 import { languageStore } from '@alpsbte/shared/stores';
 import { inject, observer } from 'mobx-react';
+import ApplicationStep from '../components/application-step/application-step';
+import { tr } from '@alpsbte/shared/language';
 
 export interface ApplicationProps {}
 
@@ -17,7 +19,42 @@ export const Application = inject(languageStore.storeKey)(
           />
         </div>
         <div className="application__content">
-          
+          <ApplicationStep
+            stepNumber={1}
+            title={tr('pages.application.steps.discord.title')}
+            description={tr('pages.application.steps.discord.description')}
+            img={
+              'https://cdn.discordapp.com/attachments/702818655750193152/881594516698726450/2021-08-29_19.04.45.png'
+            }
+          />
+          <ApplicationStep
+            stepNumber={2}
+            title={tr('pages.application.steps.plotSystem.title')}
+            description={tr('pages.application.steps.plotSystem.description')}
+            img={
+              'https://cdn.discordapp.com/attachments/702818655750193152/881594516698726450/2021-08-29_19.04.45.png'
+            }
+          />
+          <ApplicationStep
+            stepNumber={3}
+            title={tr('pages.application.steps.applicationForm.title')}
+            description={tr(
+              'pages.application.steps.applicationForm.description'
+            )}
+            img={
+              'https://cdn.discordapp.com/attachments/702818655750193152/881594516698726450/2021-08-29_19.04.45.png'
+            }
+          />
+          <ApplicationStep
+            stepNumber={4}
+            title={tr('pages.application.steps.becomingBuilder.title')}
+            description={tr(
+              'pages.application.steps.becomingBuilder.description'
+            )}
+            img={
+              'https://cdn.discordapp.com/attachments/702818655750193152/881594516698726450/2021-08-29_19.04.45.png'
+            }
+          />
         </div>
       </>
     );
