95768a23bcba0d3bb9aaa18e1b407efc036bdef0
Behemoth <simonostini@gmail.com>
2021-09-19 00:08:51 +0200

    added application title content

--- libs/application/src/lib/application.tsx
+++ libs/application/src/lib/application.tsx
@@ -14,8 +14,8 @@ export const Application = inject(languageStore.storeKey)(
       <>
         <div className="application">
           <PageTitle
-            title="Application"
-            subtitle="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,"
+            title={tr('pages.application.title')}
+            subtitle={tr('pages.application.description')}
           />
         </div>
         <div className="application__content">
