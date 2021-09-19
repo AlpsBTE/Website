828a418ff4b18054fd2cc8e44b35328849f7bad1
Behemoth <simonostini@gmail.com>
2021-09-19 15:59:58 +0200

    udpate

--- libs/error/src/lib/error.tsx
+++ libs/error/src/lib/error.tsx
@@ -4,6 +4,9 @@ import { PageTitle } from '@alpsbte/shared/components';
 import { languageStore } from '@alpsbte/shared/stores';
 import { inject, observer } from 'mobx-react';
 import { tr } from '@alpsbte/shared/language';
+import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
 export interface ErrorProps {}
 
@@ -17,6 +20,15 @@ export const Error = inject(languageStore.storeKey)(
           image="https://i.imgur.com/qTC69pF.png"
           className="error-page__page-title"
         />
+        <div className="error-page__content">
+          <Link
+            to={`/${languageStore.language}`}
+            className="error-page__content__back-home"
+          >
+            <span>{tr('pages.error.backHome')}</span>
+            <FontAwesomeIcon icon={faArrowRight} />
+          </Link>
+        </div>
       </div>
     );
   })
