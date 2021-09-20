014209733cf9d0403d6232fd7d059cad94d0c886
Behemoth <simonostini@gmail.com>
2021-09-20 17:54:13 +0200

    applied faq translations

--- libs/faq/src/lib/faq.tsx
+++ libs/faq/src/lib/faq.tsx
@@ -2,51 +2,56 @@
 import './faq.scss';
 import { PageTitle } from '@alpsbte/shared/components';
 import Accordeon from '../components/accordeon/accordeon';
+import { inject, observer } from 'mobx-react';
+import { languageStore } from '@alpsbte/shared/stores';
+import { tr } from '@alpsbte/shared/language';
 
 export interface FaqProps {}
 
-export const Faq: React.FC = (props: FaqProps) => {
-  const FaqData = [
-    {
-      title: 'Titel',
-      content: 'content',
-    },
-    {
-      title: 'Titel',
-      content: 'content',
-    },
-    {
-      title: 'Titel',
-      content: 'content',
-    },
-    {
-      title: 'Titel',
-      content: 'content',
-    },
-    {
-      title: 'Titel',
-      content: 'content',
-    },
-    {
-      title: 'Titel',
-      content: 'content',
-    },
-  ];
-  return (
-    <div>
-      <PageTitle
-        title="FAQ"
-        subtitle="Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,"
-      />
-      {FaqData.map((faqItem: any, i: number) => (
-        <Accordeon
-          title={faqItem.title}
-          key={i}
-          content={faqItem.content}
-        ></Accordeon>
-      ))}
-    </div>
-  );
-};
+export const Faq: React.FC = inject(languageStore.storeKey)(
+  observer(({}: FaqProps) => {
+    const FaqData = [
+      {
+        title: 'Titel',
+        content: 'content',
+      },
+      {
+        title: 'Titel',
+        content: 'content',
+      },
+      {
+        title: 'Titel',
+        content: 'content',
+      },
+      {
+        title: 'Titel',
+        content: 'content',
+      },
+      {
+        title: 'Titel',
+        content: 'content',
+      },
+      {
+        title: 'Titel',
+        content: 'content',
+      },
+    ];
+    return (
+      <div>
+        <PageTitle
+          title={tr('pages.faq.title')}
+          subtitle={tr('pages.faq.description')}
+        />
+        {FaqData.map((faqItem: any, i: number) => (
+          <Accordeon
+            title={faqItem.title}
+            key={i}
+            content={faqItem.content}
+          ></Accordeon>
+        ))}
+      </div>
+    );
+  })
+);
 
 export default Faq;
