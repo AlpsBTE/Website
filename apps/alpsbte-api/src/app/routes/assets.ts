b207b4305b67203a0ea62014845b906001caf1f1
Behemoth <simonostini@gmail.com>
2021-09-20 09:53:19 +0200

    added assets index get

--- apps/alpsbte-api/src/app/routes/assets.ts
+++ apps/alpsbte-api/src/app/routes/assets.ts
@@ -1,6 +1,12 @@
 import * as express from 'express';
+import { Request, Response } from 'express';
 import * as path from 'path';
+import { nameOf } from '@alpsbte/shared/util';
 
 export const assets = express.Router();
+
+assets.get('/', (req: Request, res: Response) => {
+  return res.send(nameOf({ assets }));
+});
 
 assets.use(express.static(path.join(__dirname, '/assets')));
