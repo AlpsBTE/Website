d4bf2ec06f327bd4ccbe609e3a11f032502f39c1
Gerog-code <jornig@outlook.com>
2021-10-13 17:37:24 +0200

    base

--- libs/gallery/src/lib/gallery.tsx
+++ libs/gallery/src/lib/gallery.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable*/
-import './gallery.module.scss';
+import './gallery.scss';
 import { PageTitle } from '@alpsbte/shared/components';
 import React from 'react';
 import { inject, observer } from 'mobx-react';
@@ -16,6 +16,64 @@ export const Gallery: React.FC = inject(languageStore.storeKey)(
           title={tr('pages.gallery.title')}
           subtitle={tr('pages.gallery.description')}
         />
+        <div className="gallery__container">
+          <div className="gallery__image-container">
+            <img
+              src={
+                'https://images.unsplash.com/photo-1634055614116-3a96869e21ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80'
+              }
+              className="gallery__image"
+            ></img>
+            <img
+              src={
+                'https://images.unsplash.com/photo-1634055614116-3a96869e21ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80'
+              }
+              className="gallery__image"
+            ></img>
+            <img
+              src={
+                'https://images.unsplash.com/photo-1634055614116-3a96869e21ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80'
+              }
+              className="gallery__image"
+            ></img>
+            <img
+              src={
+                'https://images.unsplash.com/photo-1634055614116-3a96869e21ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80'
+              }
+              className="gallery__image"
+            ></img>
+            <img
+              src={
+                'https://images.unsplash.com/photo-1634055614116-3a96869e21ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80'
+              }
+              className="gallery__image"
+            ></img>
+            <img
+              src={
+                'https://images.unsplash.com/photo-1634055614116-3a96869e21ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80'
+              }
+              className="gallery__image"
+            ></img>
+            <img
+              src={
+                'https://images.unsplash.com/photo-1634055614116-3a96869e21ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80'
+              }
+              className="gallery__image"
+            ></img>
+            <img
+              src={
+                'https://images.unsplash.com/photo-1634055614116-3a96869e21ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80'
+              }
+              className="gallery__image"
+            ></img>
+            <img
+              src={
+                'https://images.unsplash.com/photo-1634055614116-3a96869e21ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80'
+              }
+              className="gallery__image"
+            ></img>
+          </div>
+        </div>
       </div>
     );
   })
