8ddc8400fc7ab006230aed3ba93088f4ff960320
Gerog-code <jornig@outlook.com>
2021-10-13 19:20:27 +0200

    added Images

--- libs/gallery/src/lib/gallery.tsx
+++ libs/gallery/src/lib/gallery.tsx
@@ -19,6 +19,7 @@ export const Gallery: React.FC = inject(languageStore.storeKey)(
         <div className="gallery__container">
           <div className="gallery__image-container">
             <img
+              draggable="false"
               src={
                 'https://images.unsplash.com/photo-1634055614116-3a96869e21ed?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1470&q=80'
               }
