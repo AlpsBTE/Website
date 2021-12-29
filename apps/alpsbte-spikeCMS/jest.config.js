19edd3a429e04148bcdd6f5237347d2fad191040
Georg-code <jornig@outlook.com>
2021-12-29 03:44:39 +0100

    rename

--- apps/alpsbte-spikeCMS/jest.config.js
+++ apps/alpsbte-spikeCMS/jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  displayName: 'spike',
+  displayName: 'alpsbte-spikeCMS',
   preset: '../../jest.preset.js',
   transform: {
     '^(?!.*\\.(js|jsx|ts|tsx|css|json)$)': '@nrwl/react/plugins/jest',
     '^.+\\.[tj]sx?$': ['babel-jest', { presets: ['@nrwl/next/babel'] }],
   },
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx'],
-  coverageDirectory: '../../coverage/apps/spike',
+  coverageDirectory: '../../coverage/apps/alpsbte-spikeCMS',
 };
