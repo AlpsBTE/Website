e114d329de7bfbb7cfb18e9e0608d5fd5c79a756
Georg-code <jornig@outlook.com>
2022-01-01 16:37:58 +0100

    Revert "I dunno what i made. Why did I? Can't think anymor"
    This reverts commit a3c87e8c8fc1f602a749ca8b308252dbfb825e4c.
    
    oufff

--- apps/alpsbte-spike-cms/pages/_app.tsx
+++ apps/alpsbte-spike-cms/pages/_app.tsx
@@ -1,14 +1,12 @@
 import { SessionProvider } from "next-auth/react"
-import { NextUIProvider } from '@nextui-org/react';
+
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
   return (
-    <NextUIProvider>
     <SessionProvider session={session}>
       <Component {...pageProps} />
     </SessionProvider>
-    </NextUIProvider>
   )
 }
--- apps/alpsbte-spike-cms/pages/index.tsx
+++ apps/alpsbte-spike-cms/pages/index.tsx
@@ -1,20 +1,19 @@
-import { useSession, signIn, signOut } from 'next-auth/react';
-import { Button, Text } from '@nextui-org/react';
-import './index.module.scss';
+import { useSession, signIn, signOut } from "next-auth/react"
+
 export default function Index() {
-  const { data: session } = useSession();
+  const { data: session } = useSession()
   if (session) {
     return (
       <>
         Signed in as {session.user.email} <br />
         <button onClick={() => signOut()}>Sign out</button>
       </>
-    );
+    )
   }
   return (
-    <div className="page">
-      <Text h2>Spike - Alps Content Manager</Text>
-      <Button onClick={() => signIn()}>Sign in with Discord</Button>
-    </div>
-  );
+    <>
+      Not signed in <br />
+      <button onClick={() => signIn()}>Sign in</button>
+    </>
+  )
 }
